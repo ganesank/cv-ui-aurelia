d4094a51122ae7c8a28e235fd23546c5e35a19e6
Hannes Jörk <hannes.joerk@t-systems.com>
2020-05-11 17:57:01 +0200

    Posts moved into a router-view

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,19 @@
+import {Router, RouterConfiguration} from 'aurelia-router';
+import {PLATFORM} from 'aurelia-framework';
+
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './assets/styles/blog.css';
 
 export class App {
   public message: string = 'Hello World!';
+  router: Router;
+
+  configureRouter(config: RouterConfiguration, router){
+    config.title = 'Arise\'s Blog';
+    config.map([
+      { route: '', name: 'home', moduleId: PLATFORM.moduleName('posts/index'), title: 'All Posts'}
+    ]);
+  }
+
 }
